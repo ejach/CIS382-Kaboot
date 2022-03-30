0aff1c179dba9569b0e6e5291ffd17751bb367b0
Evan J <42357644+ejach@users.noreply.github.com>
2022-03-30 13:12:10 -0400

    Initial Commit

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    message = 'Hello, World!'
+    return render_template('index.html', message=message)
 
 
 if __name__ == '__main__':
