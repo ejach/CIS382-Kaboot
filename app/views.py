6bc67846a63c25959c6c038eace9f04fc95b4706
Evan J <42357644+ejach@users.noreply.github.com>
2022-03-30 15:50:22 -0400

    Refactoring

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():  # put application's code here
+    message = 'Hello, World!'
+    return render_template('index.html', message=message)
